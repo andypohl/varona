53b0bd5c8a66ed4b7160df7c6585f2774eb5206d
Andy Pohl <andy@pohl.io>
2024-07-09 17:09:34 -0500

    API querying

--- src/verona/ensembl.py
+++ src/verona/ensembl.py
@@ -6,12 +6,35 @@ the data for querying the Ensembl API.
 
 import contextlib
 import csv
+import enum
 import io
+import json
 import logging
 import pathlib
+import time
 import typing
 
+import httpx
 import pysam
+
+logger = logging.getLogger("verona.ensembl")
+
+
+class Assembly(enum.Enum):
+    """Enum for the assembly choice.
+
+    There are only two choices for now, GRCh37 and GRCh38.
+    """
+
+    GRCh37 = enum.auto()  # aka hg19 or human_g1k_v37
+    GRCh38 = enum.auto()  # aka hg38
+
+
+VEP_DEFAULT_PARAMS = {
+    "species": "human",
+    "variant_class": True,
+    "pick": True,
+}
 
 HTS_COMPRESSED_VALS = ("GZIP", "BGZF")
 """Possible values for the compression field in a :class:`pysam.VariantFile`.
@@ -24,12 +47,16 @@ Values in these columns are replaced with '.' before querying the
 Ensembl API.
 """
 
+API_LONG_RETRY = 60
+"""Delay retrying an API call after a 429 response without Retry-After header."""
+
 
 @contextlib.contextmanager
 def _text_from_bgzf(file_path, mode="r"):
     """Wraps a UTF-8 byte decoder around a BGZF-decompressor.
 
-    This is just a helper to open .vcf and .vcf.gz files in the
+    This is just a helper to open .vcf.gz files with the same call signature
+    as :func:`open` would on a plain .vcf file.
     """
     with pysam.BGZFile(file_path, mode) as bgzf, io.TextIOWrapper(
         bgzf, encoding="utf-8"
@@ -41,7 +68,7 @@ def vcf_rows(vcf_path: pathlib.Path) -> typing.Iterator[list[str]]:
     """Yields rows from a VCF file.
 
     This function opens a VCF file that's either plain text or compressed.
-    Normally the `:class:pysam.VariantFile` class would be used to read
+    Normally the :class:`pysam.VariantFile` class would be used to read
     VCF files but in this case there's no need to parse the VCF fully as
     VariantFile does, it's just a matter of reading tab-separated lines
     of text.
@@ -50,6 +77,7 @@ def vcf_rows(vcf_path: pathlib.Path) -> typing.Iterator[list[str]]:
     but currently it's only needed by the Ensembl API querying code.
 
     :param vcf_path: Path to the VCF file.
+    :return: Iterator of rows from the VCF file.
     """
     file_opener = open
     with pysam.VariantFile(vcf_path, "r") as vf:
@@ -65,12 +93,13 @@ def vcf_rows(vcf_path: pathlib.Path) -> typing.Iterator[list[str]]:
             yield row
 
 
-def get_vcf_query_data(
+def vcf_to_vep_query_data(
     vcf_path: pathlib.Path, chunk_size=1000
 ) -> typing.Iterator[list[str]]:
     """Gets lists of data in chunks from the VCF file for querying the Ensembl API.
 
-    Specifically, the `POST vep/:species/region <https://rest.ensembl.org/documentation/info/vep_region_post>`_
+    This is tailored to provide input to the
+    `POST vep/:species/region <https://rest.ensembl.org/documentation/info/vep_region_post>`_
     endpoint.
     """
     chunk = []
@@ -83,3 +112,72 @@ def get_vcf_query_data(
             chunk = []
     if chunk:
         yield chunk
+
+
+def query_vep_api(
+    chunk,
+    assembly: Assembly = Assembly.GRCh37,
+    retries=3,
+    params: dict | None = VEP_DEFAULT_PARAMS,
+    response_extractor: typing.Callable[[dict], dict] | None = None,
+):
+    """Queries the Ensembl Variant Effect Predictor (VEP) API.
+
+    The API endpoint is `POST vep/:species/region
+    <https://rest.ensembl.org/documentation/info/vep_region_post>`_. The API has
+    several limits on this endpoint, including a maximum of 1000 variants per
+    request, and may return a 429 status code if the rate limit is exceeded.  This
+    function can retry the request multiple times, using the Retry-After header if
+    it's present to delay the next request.
+
+    This function uses the synchronous client from the
+    `httpx <https://www.python-httpx.org/>`_ library, which is not as powerful as
+    the asynchronous client.  A future version of this function may use the
+    asynchronous client to improve performance.
+
+    :param chunk: List of (up to 1000) strings from the VCF file.  See
+        :func:`get_vcf_query_data` for the format of these strings.
+    :param assembly: Assembly to use for the API query, by default GRCh37.
+    :param retries: Number of times to retry the API call if it returns a 429 status
+        code.
+    :param params: Additional parameters to pass to the API.  By default, this
+        includes the `species=human` `variant_class=true`, and `pick=true`
+        parameters.  `pick=true` is helpful for simplifying the consequences
+        of the variants and make it easier to assign a single annotation derived
+        from the consequences to the variant.
+    :param response_extractor: Optional function to transform data from the API
+        response. Without this, there is probably more fields in the response
+        with fields without the desired names. Additionally, the default dict
+        items returned have additional structure (sublist, subdicts) that is
+        preferably flattened by this function.
+    """
+    subdomain = "grch37.rest" if assembly == Assembly.GRCh37 else "rest"
+    url = f"https://{subdomain}.ensembl.org/vep/homo_sapiens/region"
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    data = json.dumps({"variants": chunk})
+    retried = 0
+    while retried <= retries:
+        response = httpx.post(url, headers=headers, data=data, params=params)
+        if response.status_code == 200:
+            items = response.json()
+            if response_extractor:
+                # optionally transform data
+                items = list(map(response_extractor, items))
+            return items
+        elif response.status_code == 429:
+            retried += 1
+            retry_after = response.headers.get("Retry-After")
+            if retry_after:
+                sleep_time = int(retry_after)
+                logger.warn(
+                    f"API code 429 with Retry-After. Retrying after {sleep_time} seconds."
+                )
+                time.sleep(sleep_time)
+            else:
+                logger.warn(
+                    f"API code 429 with no Retry-After. Retrying after {API_LONG_RETRY} seconds."
+                )
+                time.sleep(sleep_time)
+        else:
+            response.raise_for_status()
+    raise TimeoutError("too many retries")
--- src/verona/ensembl_test.py
+++ src/verona/ensembl_test.py
@@ -1,46 +1,208 @@
+import os
 import unittest
+from unittest import mock
 
+import httpx
 import pysam
 
-from verona import ensembl, fake_vcf
+from verona import ensembl, extract, fake_vcf
+
+VERONA_LIVE_TESTS = os.getenv("VERONA_LIVE_TESTS", "0") == "1"
+"""Set to 1 to enable live querying tests for local development.
+"""
+
+
+class TestQueryVepApi(unittest.TestCase):
+
+    # A chunk of two records that'll be ignored anyway.
+    chunk = [
+        "1 1158631 . A G . . .",
+        "1 91859795 . TATGTGA CATGTGA,CATGTGG . . .",
+    ]
+    # Mock responses
+    response_429 = httpx.Response(
+        status_code=429,
+        headers={"Retry-After": "1"},
+        json={"error": "Too Many Requests"},
+    )
+    response_429_no_retry = httpx.Response(
+        status_code=429,
+        json={"error": "Too Many Requests"},
+    )
+    response_200 = httpx.Response(status_code=200, json={"data": "success"})
+
+    @mock.patch("httpx.post")
+    @mock.patch("time.sleep", return_value=None)  # disable sleeping
+    def test_query_and_retries(self, mock_sleep, mock_post):
+        mock_post.side_effect = [self.response_429, self.response_200]
+        # Call the function
+        with self.assertLogs("verona.ensembl", level="WARNING") as cm:
+            result = ensembl.query_vep_api(chunk=self.chunk)
+            self.assertEqual(len(cm.output), 1)  # Verify warning message
+            self.assertEqual(
+                cm.output[0],
+                "WARNING:verona.ensembl:API code 429 with Retry-After. Retrying after 1 seconds.",
+            )
+        self.assertEqual(result, {"data": "success"})  # Verify the result
+        self.assertEqual(mock_post.call_count, 2)  # Verify two API calls (retry)
+        mock_sleep.assert_called_once_with(1)  # Verify retry delay
+
+    @mock.patch("httpx.post")
+    @mock.patch("time.sleep", return_value=None)  # disable sleeping
+    def test_too_many_retries(self, mock_sleep, mock_post):
+        mock_post.side_effect = [
+            self.response_429,  # mock response 1
+            self.response_429,  # mock response 2
+            self.response_429,  # mock response 3
+            self.response_200,  # (doesn't get this far)
+        ]
+        # Call the function
+        with self.assertLogs("verona.ensembl", level="WARNING") as cm:
+            with self.assertRaises(TimeoutError):
+                ensembl.query_vep_api(chunk=self.chunk, retries=2)
+            self.assertEqual(len(cm.output), 3)  # Verify warning message
+            for i in range(2):
+                self.assertEqual(
+                    cm.output[i],
+                    "WARNING:verona.ensembl:API code 429 with Retry-After. Retrying after 1 seconds.",
+                )
+        self.assertEqual(mock_post.call_count, 3)
 
 
 class TestChunkReader(fake_vcf.TestWithTempDir):
 
+    # A few records to test with.
+    records = [
+        {
+            "pos": 100,
+            "alleles": ("A", "G", "C"),
+            "info": {"DP": 100},
+            "samples": [{"GT": (1, 2)}],
+        },
+        {
+            "pos": 120,
+            "alleles": ("G", "C"),
+            "info": {"DP": 222},
+            "samples": [{"GT": (1, 1)}],
+        },
+        {
+            "pos": 130,
+            "alleles": ("G", "1"),
+            "info": {"DP": 381},
+            "samples": [{"GT": (0, 1)}],
+        },
+        {
+            "pos": 140,
+            "alleles": ("A", "C"),
+            "info": {"DP": 88},
+            "samples": [{"GT": (1, 1)}],
+        },
+        {
+            "pos": 150,
+            "alleles": ("A", "G"),
+            "info": {"DP": 97},
+            "samples": [{"GT": (1, 1)}],
+        },
+    ]
+
     def test_reading_plain_vcf(self):
         """Tests reading a plain VCF file with one record."""
-        records = [
-            {
-                "pos": 100,
-                "qual": 200,
-                "alleles": ("A", "G", "C"),
-                "info": {"DP": 100},
-                "samples": [{"GT": (1, 2)}],
-            },
-        ]
-        fake = fake_vcf.FakeVcfFile(self.path, records=records)
+
+        fake = fake_vcf.FakeVcfFile(self.path, records=[self.records[0]])
         fake.write_vcf()
         self.assertTrue(fake.path.exists())
-        chunks = list(ensembl.get_vcf_query_data(fake.path))
+        chunks = list(ensembl.vcf_to_vep_query_data(fake.path))
         self.assertEqual(len(chunks), 1)
         self.assertEqual(len(chunks[0]), 1)
 
     def test_reading_gzipped_vcf(self):
         """Tests reading a bgzipped-VCF file with one record."""
-        records = [
-            {
-                "pos": 100,
-                "qual": 200,
-                "alleles": ("A", "G", "C"),
-                "info": {"DP": 100},
-                "samples": [{"GT": (1, 2)}],
-            },
-        ]
-        fake = fake_vcf.FakeVcfFile(self.path, records=records)
+        fake = fake_vcf.FakeVcfFile(self.path, records=[self.records[0]])
         fake.write_vcf()
         compressed_path = fake.path.with_suffix(".vcf.gz")
         pysam.tabix_compress(str(fake.path), str(compressed_path))
         self.assertTrue(compressed_path.exists())
-        chunks = list(ensembl.get_vcf_query_data(compressed_path))
+        chunks = list(ensembl.vcf_to_vep_query_data(compressed_path))
         self.assertEqual(len(chunks), 1)
         self.assertEqual(len(chunks[0]), 1)
+
+    def test_chunking(self):
+        """Tests the chunking of data collected from a VCF file."""
+        fake = fake_vcf.FakeVcfFile(self.path, records=self.records)
+        fake.write_vcf()
+        self.assertTrue(fake.path.exists())
+        chunks = list(ensembl.vcf_to_vep_query_data(fake.path))
+        self.assertEqual(len(chunks), 1)
+        self.assertEqual(len(chunks[0]), 5)
+        # 3 chunks if the chunk size is 2, the last chunk has 1 record
+        chunks = list(ensembl.vcf_to_vep_query_data(fake.path, chunk_size=2))
+        self.assertEqual(len(chunks), 3)
+        self.assertEqual(len(chunks[0]), 2)
+        self.assertEqual(len(chunks[1]), 2)
+        self.assertEqual(len(chunks[2]), 1)
+
+    def test_chunk_data(self):
+        """Tests the list of strings returned by `func:ensembl.vcf_to_vep_query_data`
+
+        Make sure the data is in the correct format for the Ensembl API.
+        """
+        fake = fake_vcf.FakeVcfFile(self.path, records=self.records)
+        fake.write_vcf()
+        self.assertTrue(fake.path.exists())
+        chunks = list(ensembl.vcf_to_vep_query_data(fake.path))
+        self.assertEqual(len(chunks), 1)
+        self.assertEqual(len(chunks[0]), 5)
+        expected = [
+            "1 100 . A G,C . . .",
+            "1 120 . G C . . .",
+            "1 130 . G 1 . . .",
+            "1 140 . A C . . .",
+            "1 150 . A G . . .",
+        ]
+        self.assertListEqual(chunks[0], expected)
+
+
+@unittest.skipUnless(VERONA_LIVE_TESTS, "Live querying tests are disabled by default.")
+class TestLiveQuerying(unittest.TestCase):
+    """Tests the live Ensembl API.
+
+    Normally these should be disabled. They are here for development purposes and
+    require opting-in by setting the environment variable `VERONA_LIVE_TESTS=1`.
+    """
+
+    def test_two_records(self):
+        """Tests querying the Ensembl API with two records."""
+        chunk = [
+            "1 1158631 . A G . . .",  # biallelic
+            "1 91859795 . TATGTGA CATGTGA,CATGTGG . . .",  # > 2 alleles
+        ]
+        data = ensembl.query_vep_api(
+            chunk, response_extractor=extract.default_vep_response_extractor
+        )
+        self.assertEqual(len(data), 2)
+        expected = [
+            {
+                "contig": "1",
+                "pos": 1158631,
+                "ref": "A",
+                "alt": "G",
+                "type": "SNV",
+                "effect": "synonymous_variant",
+                "gene_name": "SDF4",
+                "gene_id": "ENSG00000078808",
+                "transcript_id": "ENST00000360001",
+            },
+            {
+                "contig": "1",
+                "pos": 91859795,
+                "ref": "TATGTGA",
+                "alt": "CATGTGA,CATGTGG",
+                "type": "substitution",
+                "effect": "missense_variant",
+                "gene_name": "HFM1",
+                "gene_id": "ENSG00000162669",
+                "transcript_id": "ENST00000370425",
+            },
+        ]
+        self.assertDictEqual(data[0], expected[0])
+        self.assertDictEqual(data[1], expected[1])
